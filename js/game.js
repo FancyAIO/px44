a24099bab8bc6bc9d443d6d2deed3deee69e8643
DylanWallace12 <72448975+DylanWallace12@users.noreply.github.com>
2021-02-23 11:21:26 -0500

    updated canvas size
    canvas size changed to 1000x1000

--- js/game.js
+++ js/game.js
@@ -150,8 +150,8 @@ gameScene.gameOver = function() {
 // our game's configuration
 let config = {
   type: Phaser.AUTO,  //Phaser will decide how to render our game (WebGL or Canvas)
-  width: 640, // game width
-  height: 360, // game height
+  width: 1000, // game width
+  height: 1000, // game height
   scene: gameScene, // our newly created scene
 };
 
