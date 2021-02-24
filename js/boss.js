953f4e6a60456ae1529d7dc39b7ab339f4fc62e6
Michael Nipper <mnipper@holyghostprep.org>
2021-02-24 11:39:19 -0500

    Changed canvas size boss.js
    Now matches game.js file (1650x800)

--- js/boss.js
+++ js/boss.js
@@ -141,8 +141,8 @@ class gameScene extends Phaser.Scene {
 // our game's configuration
 let config = {
     type: Phaser.AUTO, //Phaser will decide how to render our game (WebGL or Canvas)
-    width: 1800, // game width
-    height: 900, // game height
+    width: 1650, // game width
+    height: 800, // game height
     scene: gameScene, // our newly created scene
     parent: 'main-game',
     physics: {
