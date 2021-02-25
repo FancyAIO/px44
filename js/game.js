b57f7502b9db7f9e9698209fdc85adf156bdb59d
Michael Nipper <mnipper@holyghostprep.org>
2021-02-25 13:32:30 -0500

    testing .webp type of image for background
    going to change back soon, test for this type of image

--- js/game.js
+++ js/game.js
@@ -11,7 +11,7 @@ class gameScene extends Phaser.Scene {
   }
   
   preload() {
-      this.load.image('background', 'img/bossbackground1.JPG');
+      this.load.image('background', 'img/CoopandHeston background.webp');
       this.load.image('boss', 'img/Eckerle Sprite.PNG');
       this.load.spritesheet('player', 'img/garflief.JPG', {
 
