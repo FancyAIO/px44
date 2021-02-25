00549e4cb69b3b54d5b970bb9034db9040729227
DylanWallace12 <72448975+DylanWallace12@users.noreply.github.com>
2021-02-25 14:11:09 -0500

    changed background to eck's background

--- js/game.js
+++ js/game.js
@@ -11,7 +11,7 @@ class gameScene extends Phaser.Scene {
   }
   
   preload() {
-      this.load.image('background', 'img/CoopandHeston background.webp');
+      this.load.image('background', 'img/Eck background.jpg');
       this.load.image('boss', 'img/Eckerle Sprite.PNG');
       this.load.spritesheet('player', 'img/garflief.JPG', {
 
