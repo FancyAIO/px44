32eb3c09d20ea59a819fc8da719cfaa981243afc
Michael Nipper <mnipper@holyghostprep.org>
2021-02-28 22:21:11 -0500

    added space bar bind to every game
    this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR); should allow everybody to use the spacebar as they please

--- js/game.js
+++ js/game.js
@@ -45,6 +45,7 @@ class gameScene extends Phaser.Scene {
   this.cameras.main.resetFX();
   // sets up keyboard binds
   this.cursors = this.input.keyboard.createCursorKeys();
+  this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   // setting world bounds function
   this.player.setCollideWorldBounds(true);
   }
