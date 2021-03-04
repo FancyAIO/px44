35e9a758f50d5e1c8aad8305e7f66e3bbf7fbee6
DylanWallace12 <72448975+DylanWallace12@users.noreply.github.com>
2021-03-04 09:08:14 -0500

    Added Bushek Background to Bushek scene

--- js/scenes/bushek.js
+++ js/scenes/bushek.js
@@ -80,7 +80,7 @@ class bushekScene extends Phaser.Scene {
     }
     
     preload() {
-      this.load.image('background', 'img/bushek/bossbackground1.JPG');
+      this.load.image('background', 'img/bushek/Bushek background.JPG');
       //this.load.image('eck', 'img/eckerle/Eckerle Sprite.PNG');
       this.load.image('bushek', 'img/bushek/Bushek Sprite.PNG')
       this.load.spritesheet('player', 'img/other/garflief.JPG', {
