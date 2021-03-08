4c1d729807ec2a15c132796a1adf5e9768224442
Michael Nipper <mnipper@holyghostprep.org>
2021-03-07 22:41:26 -0500

    fixed coopheston files
    took correct code and copypasted in

--- js/scenes/coopheston.js
+++ js/scenes/coopheston.js
@@ -67,129 +67,160 @@ var deleteKey;
 var enterKey;
 var escKey;
 
-class coophestonScene extends Phaser.Scene {
+class gameScene extends Phaser.Scene {
     constructor() {
         super({
-            key: 'coophestonScene',
+            key: 'gameScene',
             active: true
         });
+    
         this.cursor = new Phaser.Math.Vector2();
+    
         this.playerSpeed = 0.1;
         this.enemyMaxY = 1200;
         this.enemyMinY = 20;
+        this.timer;
         this.playerHealth = 100;
         this.healthBarX = 225;
     }
     
     preload() {
         this.load.image('background', 'img/coopheston/CoopandHeston background.webp');
-      this.load.image('eck', 'img/eckerle/Eckerle Sprite.PNG');
-      this.load.image('bushek', 'img/bushek/Bushek Sprite.PNG')
-      this.load.spritesheet('player', 'img/other/garflief.JPG', {
+        this.load.image('boss', 'img/eckerle/eckerleSprite.png');
+        //this.load.image('player', 'img/pipo-nekonin001.png');
+        this.load.image('block', 'img/other/block.png');
+        this.load.image('bean', 'img/projectiles/bean bullet.png')
+        this.load.spritesheet('player', 'img/other/garflief.JPG', {
+            frameWidth: 120,
+            frameHeight: 190,
+        });
+    }
 
-          frameWidth: 120,
-
-          frameHeight: 190,
-
-      });
-  }
-  
     create() {
-        // background
+       // background
        let bg = this.add.sprite(0, 0, 'background');
+       // change origin to the top-left of the sprite
        bg.setOrigin(0, 0);
-    // player
-     this.player = this.physics.add.sprite(40, this.sys.game.config.height / 2, 'player',);
-     // scale down player
-     this.player.setScale(0.5);
-     // enemies
-      this.eck = this.physics.add.sprite(200, this.sys.game.config.height / 2, 'eck',);
-      this.bushek = this.physics.add.sprite(400, this.sys.game.config.height / 2, 'bushek',);
-      this.eck.setScale(2);
-      this.bushek.setScale(2);
+       bg.setScale(4.0)
+
+       this.power=0;
+
+         //define our objects
+         let player = this.physics.add.sprite(this.sys.game.config.height/ 700, 775, "player");
+         this.player = player
+         //set the gravity
+         player.setGravityY(5000);
+         player.setOrigin(100, 100);
+         //place the ground
+         let groundX = this.sys.game.config.width / 2;
+         let groundY = this.sys.game.config.height * .99;
+         let ground = this.physics.add.sprite(groundX, groundY, "block");
+         //size the ground
+         ground.displayWidth = this.sys.game.config.width * 1.1;
+         //make the ground stay in place
+         ground.setImmovable();
+         //add the colliders
+         this.physics.add.collider(player, ground);
+       // scale down
+       this.player.setScale(0.4);
+        this.enemies = this.add.group({
+            key: 'boss',
+            repeat: 0,
+            setXY: {
+                x: 500,
+                y: 600,
+                stepX: 80,
+                stepY: 20
+            }
+        });
     
-        
+        // scale enemies
+        Phaser.Actions.ScaleXY(this.enemies.getChildren(), 0.5, 0.5);
+       // set speeds
+       Phaser.Actions.Call(this.enemies.getChildren(), function (enemy) {
+        enemy.speed = Math.random() * 2 + 1;
+    }, this);
+
+    // player is alive
+    this.isPlayerAlive = true;
+
+    // reset camera effects
+    this.cameras.main.resetFX();
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    oneKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
+    twoKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
+    threeKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.THREE);
+    fourKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FOUR);
+    fiveKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FIVE);
+    sixKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SIX);
+    sevenKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SEVEN);
+    eightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.EIGHT);
+    nineKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NINE);
+    zeroKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ZERO);
+    aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    bKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.B);
+    cKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
+    dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    eKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+    fKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+    gKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.G);
+    hKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.H);
+    iKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.I);
+    jKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J);
+    kKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K);
+    lKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
+    mKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
+    nKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.N);
+    oKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.O);
+    pKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+    qKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+    rKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+    sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    tKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.T);
+    uKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.U);
+    vKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.V);
+    wKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    xKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
+    yKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Y);
+    zKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+    altKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ALT);
+    backslashKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACK_SLASH);
+    backspaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACKSPACE);
+    backtickKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACKTICK);
+    openbracketKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.OPEN_BRACKET);
+    closedbracketKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.CLOSED_BRACKET);
+    capslockKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.CAPS_LOCK);
+    semicolonKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SEMICOLON);
+    commaKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.COMMA);
+    ctrlKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.CTRL);
+    deleteKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DELETE);
+    enterKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+    escKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+    f1Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F1);
+    f2Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F2);
+    f3Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F3);
+    f4Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F4);
+    f5Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F5);
+    f6Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F6);
+    f7Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F7);
+    f8Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F8);
+    f9Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F9);
+    f10Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F10);
+    f11Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F11);
+    forwardslashKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FORWARD_SLASH);
+    minusKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.MINUS);
+    periodKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.PERIOD);
+    plusKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.PLUS);
+    quotesKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.QUOTES);
+    tabKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TAB);
+
+    this.player.setCollideWorldBounds(true);
+    }
     
-        // player is alive
-        this.isPlayerAlive = true;
-        // reset camera effects
-        this.cameras.main.resetFX();
-        // sets up keyboard binds
-        this.cursors = this.input.keyboard.createCursorKeys();
-        spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        oneKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
-        twoKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
-        threeKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.THREE);
-        fourKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FOUR);
-        fiveKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FIVE);
-        sixKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SIX);
-        sevenKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SEVEN);
-        eightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.EIGHT);
-        nineKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NINE);
-        zeroKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ZERO);
-        aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        bKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.B);
-        cKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
-        dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        eKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
-        fKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-        gKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.G);
-        hKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.H);
-        iKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.I);
-        jKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J);
-        kKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K);
-        lKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
-        mKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
-        nKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.N);
-        oKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.O);
-        pKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
-        qKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
-        rKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-        sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-        tKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.T);
-        uKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.U);
-        vKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.V);
-        wKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-        xKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
-        yKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Y);
-        zKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-        altKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ALT);
-        backslashKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACK_SLASH);
-        backspaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACKSPACE);
-        backtickKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACKTICK);
-        openbracketKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.OPEN_BRACKET);
-        closedbracketKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.CLOSED_BRACKET);
-        capslockKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.CAPS_LOCK);
-        semicolonKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SEMICOLON);
-        commaKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.COMMA);
-        ctrlKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.CTRL);
-        deleteKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DELETE);
-        enterKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-        escKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
-        f1Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F1);
-        f2Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F2);
-        f3Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F3);
-        f4Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F4);
-        f5Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F5);
-        f6Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F6);
-        f7Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F7);
-        f8Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F8);
-        f9Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F9);
-        f10Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F10);
-        f11Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F11);
-        forwardslashKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FORWARD_SLASH);
-        minusKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.MINUS);
-        periodKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.PERIOD);
-        plusKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.PLUS);
-        quotesKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.QUOTES);
-        tabKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TAB);
-    
-        // setting world bounds function
-        this.player.setCollideWorldBounds(true);
-        }
-    
-        update() {
-          if (aKey.isDown) {
+    update() {
+        if (aKey.isDown) {
             this.player.x -= 10;
         }
         if (this.cursors.left.isDown) {
@@ -204,39 +235,69 @@ class coophestonScene extends Phaser.Scene {
 
         if (Phaser.Input.Keyboard.JustDown(spaceKey) && this.player.y > 8200) {
             this.startJump();
-            console.log("jump");
         }
 
         if (Phaser.Input.Keyboard.JustUp(spaceKey)) {
             this.endJump();
         }
+
+        // only if the player is alive
         if (!this.isPlayerAlive) {
-          return;
-      }
-              //eck collision
-      if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.eck.getBounds())) {
-        this.gameOver();
-        this.scene.start("coophestonScene");
-      }
-     //bushek collision
-     if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.bushek.getBounds())) {
-        this.gameOver();
-        this.scene.start("petcaughScene");
-      }
+            return;
         }
-  
-    gameOver() {
-      this.player.x = 40;
-      this.player.y = this.sys.game.config.height / 2;
+
+        // enemy movement
+        let enemies = this.enemies.getChildren();
+        let numEnemies = enemies.length;
+
+        for (let i = 0; i < numEnemies; i++) {
+
+            // move enemies
+            enemies[i].x += enemies[i].speed;
+
+            // reverse movement if reached the edges
+            if (enemies[i].x >= this.enemyMaxY && enemies[i].speed > 0) {
+                enemies[i].speed *= -1;
+            } else if (enemies[i].x <= this.enemyMinY && enemies[i].speed < 0) {
+                enemies[i].speed *= -1;
+            }
+
+            // enemy collision
+            if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), enemies[i].getBounds())) {
+                healthBar(this);
+                this.gameOver();
+                break;
+            }
+        }
+}
+    startJump() {
+        this.timer = this.time.addEvent({
+            delay: 0,
+            callback: this.tick,
+            callbackScope: this,
+            loop: true
+        });
+}
+    endJump() {
+        this.timer.remove();
+        this.player.setVelocityY(-this.power * 10);
+        this.power = 0;
+}
+    tick() {
+        if (this.power < 200) {
+            this.power += 200;
+        }
+}
+    gameOver() {       
     }
-  }
-  
-  // our game's configuration
-  let config = {
+}
+
+// our game's configuration
+let config = {
     type: Phaser.AUTO, //Phaser will decide how to render our game (WebGL or Canvas)
     width: 1350, // game width
     height: 750, // game height
-    scene: coophestonScene, // our newly created scene
+    scene: gameScene, // our newly created scene
     parent: 'main-game',
     physics: {
         default: 'arcade',
@@ -244,17 +305,14 @@ class coophestonScene extends Phaser.Scene {
             gravity: false
         }
     }
-    };
-    
+    };    
     // create the game, and pass it the configuration
     let game = new Phaser.Game(config);
 
-
-    function healthBar(scene) {
-      if (xKey.isDown && scene.playerHealth >= 0) {
-          scene.rect = scene.add.rectangle(scene.healthBarX, 75, scene.playerHealth * 4, 65, 0x00ff00).setStrokeStyle(4, 0x000000);
-          scene.playerHealth -= 0.5;
-          scene.healthBarX -= 1;
-      }
-  }
-  
+function healthBar(scene) {
+    if (scene.playerHealth >= 0) {
+        scene.rect = scene.add.rectangle(scene.healthBarX, 75, scene.playerHealth * 4, 65, 0x00ff00).setStrokeStyle(4, 0x000000);
+        scene.playerHealth -= 0.5;
+        scene.healthBarX -= 1;
+    }
+}
